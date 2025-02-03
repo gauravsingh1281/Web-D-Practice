51c743a48748ac14aed4a2b3121aac11e50539b7
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-03 23:56:08 +0530

    Alternate solution added.

--- Olevel Practice/Q17.py
+++ Olevel Practice/Q17.py
@@ -1,6 +1,16 @@
 # Wap to print the sum of the digits of a given no.
 N = input("Enter no. to find the sum of its digits")
 sum = 0
-for digit in N:
-    sum = sum + int(digit)
+digit = int(N)
+
+# 1st way
+# for digit in N:
+#     sum = sum + int(digit)
+# print(f"Sum of digit of {N} = {sum}")
+
+# 2nd way
+
+while digit > 0:
+    sum = sum + digit % 10
+    digit = digit // 10
 print(f"Sum of digit of {N} = {sum}")
