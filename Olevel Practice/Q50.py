2ad63ab79a74674a8e4c2098878c508ebc2b420d
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-11 03:38:35 +0530

    revision 5 solved.

--- Olevel Practice/Q50.py
+++ Olevel Practice/Q50.py
@@ -38,3 +38,16 @@ else:
         series.append(series[-2] + series[-1])
 for j in series:
     print(j)
+
+# 5 WAP to find the perfect number
+n = int(input("Enter number: "))
+i = 1
+sum = 0
+while i < n:
+    if n % i == 0:
+        sum += i
+    i += 1
+if sum == n:
+    print("It is a perfect number")
+else:
+    print("It is not a perfect number")
