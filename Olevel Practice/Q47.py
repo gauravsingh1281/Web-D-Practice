34997ac80fde164781166f23054d556714a238b9
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-10 18:06:02 +0530

    Q47 Sovled.

--- Olevel Practice/Q47.py
+++ Olevel Practice/Q47.py
@@ -1,5 +1,10 @@
-# WAP to display numbers from 0 to 6 except 3 and 6
-for i in range(0, 7):
-    if i == 3 or i == 6:
-        continue
-    print(i)
+# pattern printing 1
+N = int(input("Enter no. of row"))
+i = 1
+while i <= N:
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()
+    i += 1
