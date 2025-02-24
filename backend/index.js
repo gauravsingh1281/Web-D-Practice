98a40a2ddc7ce37a2f6cc50ff10d0e7839b78868
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-24 22:10:02 +0530

    rev 2

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const _dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile(_dirname + "/public/index.html");
 });
 
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(req.body.username);
+});
 app.listen(port, () => {
-  console.log(`app started listening on port ${port}`);
+  console.log("server started.");
 });
