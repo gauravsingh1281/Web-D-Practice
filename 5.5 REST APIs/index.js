e498740f3fc18ceb035c161f647a862389abc55b
Gaurav Pratap Singh <gs492979@gmail.com>
2025-03-11 15:50:19 +0530

    patch route added.

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -69,7 +69,23 @@ app.post("/put-secret", async (req, res) => {
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  const SECRET = req.body.secret;
+  const SCORE = req.body.score;
+  try {
+    const result = await axios.patch(
+      API_URL + "/secrets/" + searchId,
+      {
+        secret: SECRET,
+        score: SCORE,
+      },
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", {
+      content: JSON.stringify(error.response.data),
+    });
+  }
 });
 
 app.post("/delete-secret", async (req, res) => {
